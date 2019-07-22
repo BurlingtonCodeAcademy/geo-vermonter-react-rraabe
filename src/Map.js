ba89616e73434c089d84aa1d4666386a37c4f6a5
rraabe <49948103+rraabe@users.noreply.github.com>
2019-07-22 09:26:04 -0400

    Moved everything into App. Needs CSS and guessing functionality

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,6 @@
 import React from "react";
 import L from "leaflet";
 import borderData from "./bordervt"
-import countyData from "./border"
-import leafletPip from "@mapbox/leaflet-pip"
 
 const style = {
   height: "500px",
@@ -11,6 +9,7 @@ const style = {
 };
 
 class Map extends React.Component {
+
   componentDidMount() {
     // create map
     this.map = L.map("map", {
@@ -26,39 +25,47 @@ class Map extends React.Component {
               "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
           }
         ), 
-
       ]
     });
 
     // add marker
     this.marker = L.marker(this.props.markerPosition).addTo(this.map);
+
     this.borderData = L.geoJSON(borderData);
     this.borderData.addTo(this.map);
-    this.countyData = L.geoJSON(countyData);
-    this.countyData.addTo(this.map);
 
+    //County data is now in the index.js
+    this.props.countyLayer.addTo(this.map);
+  
+    //
+    
+    
     // this.map.zoomControl.remove();
     // this.map.scrollWheelZoom.disable();
     // this.map.touchZoom.disable();
     // this.map.dragging.disable();
     // this.map.keyboard.disable();
   }
+  
   //the new markerPosition gets passed in and compared to the old marker position in this.props.markerPosition
-  componentDidUpdate({ markerPosition }) {
+  componentDidUpdate({ markerPosition, startPosition }) {
     // check if position has changed
+    this.map.panTo(this.props.startPosition);
     if (this.props.markerPosition !== markerPosition) {
       this.marker.setLatLng(this.props.markerPosition);
       this.map.panTo(this.props.markerPosition);
 
-      const result = leafletPip.pointInLayer([markerPosition.lon, markerPosition.lat], this.countyData);
-      console.log(result[0].feature.properties.CNTYNAME);
+      // const result = leafletPip.pointInLayer([startPosition.lon, startPosition.lat], this.countyData);
+      // (result.length > 0) ? console.log(result[0].feature.properties.CNTYNAME) : console.log('result array is empty');
     }
-
   }
   
   render() {
+    
     return <div id="map" style={style} />;
   }
 }
+
+ 
 
 export default Map;
--- src/index.js
+++ src/index.js
@@ -1,94 +1,152 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Map from "./Map.js";
-import countyMap from './assets/county-map.jpg';
+import countyMap from "./assets/county-map.jpg";
+import countyData from "./border";
+import leafletPip from "@mapbox/leaflet-pip";
+import L from "leaflet";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.countyLayer = this.makeCountyLayer();
+    this.startPosition = this.pickRandomLatLon();
     this.state = {
-      startPosition: { lat: 44.4759406, lon: -73.2123868 },
-      markerPosition: { lat: 44.477272, lon: -73.2123868 }
+      markerPosition: this.startPosition,
+      score: 1000,
+      gameStarted: false,
+      startingCounty: null,
+      //guessedCounty passed from dropdown
     };
     this.moveNorth = this.moveNorth.bind(this);
     this.moveEast = this.moveEast.bind(this);
-    this.moveSouth= this.moveSouth.bind(this);
+    this.moveSouth = this.moveSouth.bind(this);
     this.moveWest = this.moveWest.bind(this);
+    this.pickRandomLatLon = this.pickRandomLatLon.bind(this);
+    this.makeCountyLayer = this.makeCountyLayer.bind(this);
+    this.checkStart = this.checkStart.bind(this);
+    
+  }
+  
+  makeCountyLayer() {
+    let countyLayer = L.geoJSON(countyData);
+    return countyLayer;
+  }
 
+  pickRandomLatLon() {
+    let { lat, lon } = {lat: 0, lon: 0};
+    while ((this.checkStart({lat, lon})).length<1) {
+      console.log('in the while')
+      lat = Math.random() * (45.04 - 42.7) + 42.7;
+      lon = -1 * (Math.random() * (73.5 - 71.4) + 71.4);
+      this.checkStart({ lon: lon, lat: lat });
+    }
+    
+    return { lat: lat, lon: lon };
+  }
+
+  checkStart(point) {
+    let startingPoint = leafletPip.pointInLayer([point.lon, point.lat], this.countyLayer);
+    if(startingPoint.length>0)
+    {
+      console.log(startingPoint[0].feature.properties.CNTYNAME);
+    }
+    return startingPoint;
   }
 
   moveNorth() {
     const { lat, lon } = this.state.markerPosition;
     this.setState({
       markerPosition: {
-        lat: lat + 0.0003,
+        lat: lat + 0.0001,
         lon: lon
-      }
+      },
+      score: this.state.score - 10
     });
   }
-  
+
   moveEast() {
     const { lat, lon } = this.state.markerPosition;
     this.setState({
       markerPosition: {
         lat: lat,
         lon: lon + 0.0001
-      }
+      },
+      score: this.state.score - 10
     });
   }
-  
+
   moveSouth() {
     const { lat, lon } = this.state.markerPosition;
     this.setState({
       markerPosition: {
         lat: lat - 0.0001,
         lon: lon
-      }
+      },
+      score: this.state.score - 10
     });
   }
-  
+
   moveWest() {
     const { lat, lon } = this.state.markerPosition;
     this.setState({
       markerPosition: {
         lat: lat,
         lon: lon - 0.0001
-      }
+      },
+      score: this.state.score - 10
     });
   }
-  
-  
-
+  componentDidMount(){
+    let countyArrayObj = leafletPip.pointInLayer([this.startPosition.lon, this.startPosition.lat], this.countyLayer);
+    console.log(countyArrayObj[0].feature.properties.CNTYNAME);
+    this.setState({
+      startingCounty: countyArrayObj[0].feature.properties.CNTYNAME
+    });
+  }
   render() {
     const { markerPosition } = this.state;
-    const { startPosition } = this.state;
 
-    return (
-      <div>
-        <Nav />
-        <div className="horizontal-flex-box">
-          <div className="vertical-flex-box">
-            <button className="nav-button" onClick={this.moveNorth}><i class="fas fa-chevron-up"></i></button>
-            <button className="nav-button" onClick={this.moveEast}><i class="fas fa-chevron-right"></i></button>
-            <button className="nav-button" onClick={this.moveSouth}><i class="fas fa-chevron-down"></i></button>
-            <button className="nav-button" onClick={this.moveWest}><i class="fas fa-chevron-left"></i></button>
-            <Map
-              markerPosition={markerPosition}
-              startPosition={startPosition}
-            />
-            <InfoBox />
+    // const { startPosition } = this.state;
+      return (
+        <div>
+          <Nav />
+          <div className="horizontal-flex-box">
+            <div className="vertical-flex-box">
+              <button className="nav-button" onClick={this.moveNorth}>
+                <i class="fas fa-chevron-up" />
+              </button>
+              <button className="nav-button" onClick={this.moveEast}>
+                <i class="fas fa-chevron-right" />
+              </button>
+              <button className="nav-button" onClick={this.moveSouth}>
+                <i class="fas fa-chevron-down" />
+              </button>
+              <button className="nav-button" onClick={this.moveWest}>
+                <i class="fas fa-chevron-left" />
+              </button>
+              <Map
+                markerPosition={markerPosition}
+                startPosition={this.startPosition}
+                pickRandomLatLon={this.pickRandomLatLon}
+                countyLayer={this.countyLayer}
+              />
+              <InfoBox />
+            </div>
+            <img id="county-map" src={countyMap} alt="Vermont Counties Map" />
           </div>
-          <img id="county-map" src={countyMap} alt="Vermont Counties Map"></img>
+          <div id="button-row">
+            <button id="start-button" onClick={this.pickRandomLatLon  }>
+              Start
+            </button>
+            <button id="one-button">One</button>
+            <button id="two-button">Two</button>
+            <button id="three-button">Three</button>
+          </div>
+          <div>You're score is: {this.state.score}</div>
         </div>
-        <div id="button-row">
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        </div>
-        <Score />
-      </div>
-    );
+      );
+    
   }
 }
 
@@ -99,15 +157,6 @@ const Nav = props => {
 const InfoBox = props => {
   //Needs fields for latitude, long, county, and town
   return <h1>Info Element</h1>;
-};
-const Button = props => {
-  //Many buttons:
-  //Move N, S, E, W
-  //Start, guess, quit
-  return <button>A Button</button>;
-};
-const Score = props => {
-  return <h1>Score</h1>;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
